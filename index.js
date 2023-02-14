80f0c9787ba47d193affba4285936b5ca5715dfe
Dalton Liebl <90783550+DaltonBellini@users.noreply.github.com>
2023-02-14 11:52:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,71 @@
-let randomPhrase = Math.round(Math.random()*10)
-const phrase = document.querySelector("#phrase")
-const screen1 = document.querySelector(".screen1")
-const screen2 = document.querySelector(".screen2")
-const btnCookie = document.querySelector("#buttonCookie")
+
+const arrayMessage = [
+    "A vida é feita de pequenos momentos de felicidade.",
+    "Mantenha a fé, as coisas vão dar certo.",
+    "A esperança é a última que morre.",
+    "Tudo acontece por um motivo.",
+    "A vida é curta, sorria enquanto pode.",
+    "Os desafios são oportunidades para se mostrar o que você é capaz.",
+    "A persistência é a chave do sucesso.",
+    "O sucesso é uma jornada, não um destino.",
+    "Oportunidades são escondidas dentro de cada fracasso.",
+    "O impossível é apenas uma opinião, não uma realidade.",
+    "A felicidade é uma escolha, escolha ser feliz.",
+    "Não importa o tamanho do problema, a atitude é tudo.",
+    "A gratidão é a chave para a felicidade.",
+    "O amor é a força mais poderosa do mundo.",
+    "A determinação é a chave para vencer.",
+    "A bondade é a chave para a paz interior.",
+    "A paciência é a chave para a sabedoria.",
+    "A verdadeira riqueza é a amizade e a família.",
+    "Otimismo é a chave para uma vida feliz.",
+    "O sucesso é a soma de pequenos esforços repetidos diariamente.",
+    "A honestidade é a chave para a confiança.",
+    "A generosidade é a chave para a felicidade dos outros.",
+    "A humildade é a chave para a sabedoria.",
+    "A simplicidade é a chave para a felicidade.",
+    "A confiança é a chave para o sucesso.",
+    "A coragem é a chave para superar medos e obstáculos.",
+    "A tolerância é a chave para a paz.",
+    "A empatia é a chave para o entendimento dos outros.",
+    "A sabedoria vem com a idade e a experiência.",
+    "A criatividade é a chave para o sucesso."
+];
+
+const cookieMessage = document.querySelector('#phrase')
+const screen1 = document.querySelector('.screen1')
+const screen2 = document.querySelector('.screen2')
+const btnCookie = document.querySelector('#buttonCookie')
 const btnNewCookie = document.querySelector('#btnNewCookie')
-
-const zeroo = Number(0)
-const one = Number(1)
-const two = Number(2)
-const three = Number(3)
-const four = Number(4)
-const five = Number(5)
-const six = Number(6)
-const seven = Number(7)
-const eight = Number(8)
-const nine = Number(9)
-const ten = Number(10)
+const arrayPosition = arrayMessage.length
+let randomPhrase = Math.round(Math.random() * arrayPosition)
+const phrase = document.querySelector('#phrase')
 
 
-
-btnCookie.addEventListener('click',logicSoftware)
-
-btnNewCookie.addEventListener('click',newPhrase)
-
+    // events
+btnCookie.addEventListener('click', logicSoftware) 
+btnNewCookie.addEventListener('click',logicSoftware)
 document.addEventListener('keydown',pressEnter)
-
-
-function newPhrase(event){
-    randomPhrase = Math.round(Math.random()*10)
-    logicSoftware(event)
-}
 
 
 function pressEnter(event){
     if(event.key == 'Enter' && screen2.classList.contains('hide') || screen1.classList.contains('hide')){
-        newPhrase(event)
+        logicSoftware(event)
     }
 }
 
+function logicSoftware(event){
+    event.preventDefault()
+    hideScreen()
+    newPhrase()
+    
+}
 
 
+function newPhrase(){
+    randomPhrase = Math.round(Math.random() * arrayPosition)
+    phrase.innerHTML = arrayMessage[randomPhrase]
+}
 
 
 function hideScreen(){
@@ -47,100 +73,9 @@ function hideScreen(){
     screen2.classList.toggle('hide')
 }
 
-function logicSoftware(event){
-    event.preventDefault()
-
-    hideScreen()
-    
-    if(randomPhrase == zeroo){
-        zero()
-    } 
-
-    else if(randomPhrase == one){
-        phraseOne()
-    }
-
-    if(randomPhrase == two){
-        phraseTwo()
-    }
-
-    else if(randomPhrase == three){
-        phraseThree
-    }
-
-    if(randomPhrase == four){
-        phraseFour()
-    }
-
-    else if(randomPhrase == five){
-        phraseFive()
-    }
-
-    if(randomPhrase == six){
-        phraseSix()
-    }
-    
-    else if(randomPhrase == seven){
-        phraseSeven()
-    }
-
-    if(randomPhrase == eight){
-        phraseEight()
-    }
-
-    else if (randomPhrase == nine){
-        phraseNine()
-    }
-
-    if(randomPhrase == ten){
-        phraseTen()
-    }
 
 
-}
 
 
-function zero(){
-    phrase.innerHTML = `Para ter sucesso, você deve eliminar dúvidas, aceitar desafios e rejeitar qualquer negatividade externa.`
-}
 
-function phraseOne(){
-    phrase.innerHTML = `Viver cada dia como se fosse o último é uma ótima oportunidade para aprender a amar cada segundo do seu dia.`
-}
-
-function phraseTwo(){
-    phrase.innerHTML = ` Se o plano não funcionar, mude o plano, não a meta.`
-}
-
-function phraseThree(){
-    phrase.innerHTML = `Você é mais valente do que acredita, mais forte do que parece e está mais preparado do que imagina.`
-}
-
-function phraseFour(){
-    phrase.innerHTML = ` Que os seus sonhos te levem para onde o seu coração seja feliz.`
-}
-
-function phraseFive(){
-    phrase.innerHTML = `Para ter sucesso, você deve eliminar dúvidas, aceitar desafios e rejeitar qualquer negatividade externa.`
-}
-
-function phraseSix(){
-    phrase.innerHTML = `O que você procura está procurando você.`
-}
-
-function phraseSeven(){
-    phrase.innerHTML = `Uma meta é um sonho com um prazo.`
-}
-
-function phraseEight(){
-    phrase.innerHTML = `Tente mover o mundo - o primeiro passo será mover a si mesmo.`
-}
-
-function phraseNine(){
-    phrase.innerHTML = `Disciplina é a ponte entre metas e realizações.`
-}
-
-function phraseTen(){
-    phrase.innerHTML = `Você é mais forte do que as suas desculpas.`
-}
 
